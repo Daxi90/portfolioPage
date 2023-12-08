18ef55a9b10a5bae4cea781e51e6a7eb263305ca
Daxi90 <hdateam90@gmail.com>
2023-12-08 15:28:29 +0100

    Started with above the fold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderMobileComponent
+    HeaderMobileComponent,
+    AboveTheFoldComponent
   ],
   imports: [
     BrowserModule,
